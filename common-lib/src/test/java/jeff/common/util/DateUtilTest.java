3cf4a24cd2e541bf4b01f98c5973a4351bb82f39
Jeff33759 <and14753@gmail.com>
2023-06-25 14:43:30 +0800

    日期共用包的測試重構

--- common-lib/src/test/java/jeff/common/util/DateUtilTest.java
+++ common-lib/src/test/java/jeff/common/util/DateUtilTest.java
@@ -1,8 +1,8 @@
 package jeff.common.util;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,34 +10,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.sql.Timestamp;
 import java.time.ZoneId;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@SpringBootConfiguration
+@SpringBootTest(classes = DateUtil.class)
 class DateUtilTest {
 
     @Spy
     DateUtil spyDateUtil; //待測元件
 
-    String timeString;
-    long matchTimestamp;
+    String stubTimeString;
+    long stubMatchTimestamp;
+
+    @AfterEach
+    void resetAllArgsForTestCase() {
+        this.stubTimeString = null;
+        this.stubMatchTimestamp = 0;
+    }
 
     @Test
     void GivenTimestamp_WhenConvertTimestampToUTCTimeZoneISOString_ThenReturnExpectedString() {
         prepareArgsForTestCase("2022-05-13T16:00:00.000Z", 1652457600000L);
 
-        String actual = spyDateUtil.convertTimestampToUTCTimeZoneISOString(new Timestamp(this.matchTimestamp));
+        String actual = spyDateUtil.convertTimestampToUTCTimeZoneISOString(new Timestamp(this.stubMatchTimestamp));
 
-        Assertions.assertEquals(this.timeString, actual);
+        Assertions.assertEquals(this.stubTimeString, actual);
     }
 
     @Test
     void GivenTimeFormatString_WhenConvertTimeFormatStringToTimestamp_ThenReturnExpectedTimestamp() {
         prepareArgsForTestCase("2022-05-14 00:00:00", 1652457600000L);
 
-        Timestamp actual = spyDateUtil.convertTimeFormatStringToTimestamp(this.timeString, ZoneId.systemDefault()); //開發環境電腦的系統時區為UTC+8
+        Timestamp actual = spyDateUtil.convertTimeFormatStringToTimestamp(this.stubTimeString, ZoneId.systemDefault()); //開發環境電腦的系統時區為UTC+8
 
-        Assertions.assertEquals(new Timestamp(this.matchTimestamp), actual);
+        Assertions.assertEquals(new Timestamp(this.stubMatchTimestamp), actual);
     }
 
     /**
@@ -45,8 +48,8 @@ class DateUtilTest {
      * @param matchTimestamp    時間格式字串所對應的時間戳
      */
     private void prepareArgsForTestCase(String timeString, long matchTimestamp) {
-        this.timeString = timeString;
-        this.matchTimestamp = matchTimestamp;
+        this.stubTimeString = timeString;
+        this.stubMatchTimestamp = matchTimestamp;
     }
 
 }
