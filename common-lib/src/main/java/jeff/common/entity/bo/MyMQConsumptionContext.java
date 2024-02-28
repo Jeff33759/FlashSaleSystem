c832944ca004a61ab40c276d200eeff2a78d3596
Jeff33759 <and14753@gmail.com>
2024-02-28 19:45:08 +0800

    新增關於UUID和MsgId的註解說明

--- common-lib/src/main/java/jeff/common/entity/bo/MyMQConsumptionContext.java
+++ common-lib/src/main/java/jeff/common/entity/bo/MyMQConsumptionContext.java
@@ -7,6 +7,10 @@ public class MyMQConsumptionContext extends MyContext {
 
     /**
      * 自己寫一個鏈式setter，比較好用。
+     *
+     * 此UUID不等於MsgId。
+     * MsgId是紀錄在msg裡面，代表一個msg從生產到被消費的生命週期。
+     * 此UUID則是代表consumer從接收到MSG後進行的一連串業務行為的生命週期。
      */
     public MyMQConsumptionContext chainSetUUID(String UUID) {
         super.setUUID(UUID);
